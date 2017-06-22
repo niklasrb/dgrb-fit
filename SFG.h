edfcb554a1909f2c0dacc9b64080a670fcf29051
Niklas Becker <niklasbecker@posteo.de>
2017-06-22 13:32:21 +0200

    clean up

--- SFG.h
+++ SFG.h
@@ -9,34 +9,68 @@
 class SFG : public AstrophysicalSource
 {
 protected:
-
+	double gamma_X;
+	double alpha;
+	double beta;
+	double sigma;
+	double k_L1;
+	double k_L2;
+	double z_L;
+	double k_Phi1;
+	double k_Phi2;
+	double z_Phi;
+	double L_IR_0; // maybe obsolete
+	double Phi_s_0;
 	
 public:
 	SFG(std::shared_ptr<CosmologyModel> _CM) : AstrophysicalSource(_CM, std::string("SFG"))
 	{
 		m_zBounds.first = 1; m_zBounds.second = 6;
-		m_GammaBounds.first = ; m_GammaBounds.second = ;
+		m_GammaBounds.first = 0; m_GammaBounds.second = 0;
 	}
 	
 	double kCorrection(const double E, const double z, const double Gamma) override
 	{
-
+		if(E*(1+z) < 0.6_GeV)
+			return powf(1+z, 2-1.5);
+		if(E < 0.6_GeV  && 0.6_GeV < E*(1+z))
+			return powf(1+z, 2-gamma_X) * powf(E/0.6_GeV, 1.5 - gamma_X);
+		if(0.6_GeV <= E)
+			return powf(1+z, 2-gamma_X);
 	}
 	
 	double EnergySpectrum(const double E, const double z, const double Gamma) override
 	{
+		return powf(E/0.6_GeV, ( E < 0.6_GeV ? -1.5 : -gamma_X ));
 	}
 	
 	double GammaDistribution(const double Gamma) override
 	{
-
+		return 1;
 	}			
 	
-	double LuminosityFunction(const double Luminosity, const double z, const double Gamma) override
-	{
-
-	}
 	
+	double LuminosityFunction(const double Luminosity, const double z, const double Gamma) override
+	{	// this is equivalent to the rescaled luminosity function, because GammaDistribution = 1
+		const double L_solar = 1; // find out
+		return infraredLuminosityFunction( exp(alpha* Luminosity/(L_solar*1e10) + beta), z, Gamma)/alpha; // check
+	}
+
+protected:
+	double infraredLuminosityFunction(const double Luminosity, const double z, const double Gamma) // check
+	{
+		double L_IR, Phi_s;
+		if(z < z_L) 	
+		{
+			L_IR = L_IR_0 * powf(1+z, k_L1);
+			Phi_s = Phi_s_0 * powf(1+z, k_Phi1);
+		}	else  
+		{
+			L_IR = L_IR_0 * powf(1+z_L, k_L1 - k_L2) * powf(1+z, k_L2);
+			Phi_s = Phi_s_0 * powf(1+z_Phi, k_Phi1 - k_Phi2) * powf(1+z, k_Phi2);
+		}
+		return Phi_s * powf(Luminosity/L_IR, 1-alpha) * exp(-powf(log(1+ Luminosity/L_IR), 2) /(2*sigma*sigma));
+	}
 
 };
 
