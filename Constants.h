bd7906e7419d2b603b696d9662f2791b2d3b3935
Niklas Becker <niklasbecker@posteo.de>
2017-07-21 04:43:33 +0200

    removed Bins

--- Constants.h
+++ Constants.h
@@ -1,11 +1,20 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+#include <tuple>
+
 //Speed of light km/s
 const double c_0 = 2.9979e5;
 
-//Hubble constant  km / s*MPc
+//Hubble constant in km / s*MPc
 const double H_0 = 67.74;
+
+// Gravtity constant in km^3 / kg*s^2
+const double G = 6.67e-20;
+
+// Solar Mass in kg
+const double M_solar = 1.988e20;
+
 
 // Energy reference
 const double GeV = 1;
@@ -16,6 +25,11 @@ constexpr  double operator "" _GeV(unsigned long long d) { return d; }
 constexpr double operator "" _photonspercm2s1(long double d) { return d; }
 
 // Luminosity reference
-constexpr double operator "" _erg(long double d) { return d; }
+constexpr double operator "" _ergpers(long double d) { return d; }
+
+typedef std::pair<double, double> Bounds ;			// To store Integration Bounds (left, right)
+
+
+
 
 #endif
