25141c19e6bcdc5158c599dc0698579fd1963a9d
Niklas Becker <niklas.becker1@rwth-aachen.de>
2017-07-29 02:07:47 +0200

    simple graphing

--- AstrophysicalSource.h
+++ AstrophysicalSource.h
@@ -12,6 +12,7 @@
 
 #include "Constants.h"
 #include "Source.h"
+#include "TGraph.h"
 
 
 
@@ -40,6 +41,20 @@ public:
 	virtual double RescaledLuminosityFunction(const double Luminosity, const double z, const double Gamma);
 	
 	std::vector<std::pair<Bounds, std::shared_ptr<gsl1DInterpolationWrapper> > > APS;		// Contains pair of Energy bin and C_p(S_t_1) Spline
+	
+	void printResults(double S_t_1);
+	
+	TGraph* MakeGraph()
+	{
+		std::vector<double> x, y;
+		for(unsigned int i = 0; i < Intensity.size(); i++) 
+		{
+			x.push_back((Intensity[i].first.first + Intensity[i].first.second)/2.);
+			y.push_back(Intensity[i].second);
+			//std::cout << x[i] << '\t' << y[i] << std::endl;
+		}
+		return new TGraph(x.size(), x.data(), y.data());
+	}
 };
 
 // Preimplemented functions that use the other implementations
@@ -51,6 +66,17 @@ double AstrophysicalSource::RescaledLuminosityFunction(const double Luminosity, 
 double AstrophysicalSource::EnergySpectrum(const double E, const double z, const double Gamma)
 {
 	return literal_F(E, z, Gamma)*exp(-(*tau)(E, z));
+}
+
+void AstrophysicalSource::printResults(double S_t_1)
+{
+	std::cout << Name <<  "  - Bin: Intensity" << std::endl;
+	for(unsigned int i = 0; i < Intensity.size(); i++) 
+		std::cout << "[" << Intensity.at(i).first.first << "," << Intensity.at(i).first.second <<"]:" << Intensity.at(i).second << '\t';
+	std::cout << std::endl << "Bin: C_p  ";
+	for(unsigned int i = 0; i < APS.size(); i++) 
+		std::cout << "[" << APS.at(i).first.first << "," << APS.at(i).first.second <<"]:" << APS.at(i).second->Eval(S_t_1) << '\t';
+	std::cout << std::endl;
 }
 
 #include "MAGN.h"
