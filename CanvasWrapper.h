109dee4820dcfe565662f53e6400503070bb2207
Niklas Becker <niklas.becker1@rwth-aachen.de>
2017-08-16 23:05:35 +0200

    range workgin

--- CanvasWrapper.h
+++ CanvasWrapper.h
@@ -11,30 +11,58 @@
 #include "TROOT.h"
 #include "TGraph.h"
 #include "TMultiGraph.h"
+#include "TF1.h"
+#include "TH1F.h"
 #include "TCanvas.h"
 #include "TLegend.h"
+#include "TAxis.h"
 
 
-
+/// A little class that saves the pointer to the canvas
+/// and to the graphs on it
 class Canvas
 {
 protected:
-	std::shared_ptr<TCanvas> canv;
+	std::shared_ptr<TCanvas> canv;		// shared pointer for easy memory management
 	std::shared_ptr<TMultiGraph> mg;
 public:
-	std::vector<TGraph*> Graphs;
 	
 	Canvas(const std::string name, const std::string title, int height = 500, int width = 500)
 	{
 		canv = std::make_shared<TCanvas>(name.c_str(), title.c_str(), height, width);
 		mg = std::make_shared<TMultiGraph>(); 
+		//Draw();
 	}
 	
 	void AddGraph(TGraph* g, const std::string title, const std::string drawOptions)
 	{
 		g->SetTitle(title.c_str());
-		Graphs.push_back(g);
 		mg->Add(g, drawOptions.c_str());
+		Draw();
+	}
+	
+	void SetxLimits(double xmin, double xmax)
+	{
+		mg->GetHistogram()->GetXaxis()->SetRangeUser(xmin, xmax);	
+		mg->GetXaxis()->SetLimits(xmin, xmax);
+		canv->Modified();
+		canv->Update();
+	}
+	
+	void SetyLimits(double ymin, double ymax)
+	{
+		mg->GetHistogram()->GetYaxis()->SetRangeUser(ymin, ymax);
+		mg->GetYaxis()->SetLimits(ymin, ymax);
+		canv->Modified();
+		canv->Update();
+	}
+	
+	void SetAxesTitle(std::string xAxis, std::string yAxis)
+	{
+		mg->GetXaxis()->SetTitle(xAxis.c_str());
+		mg->GetYaxis()->SetTitle(yAxis.c_str());
+		canv->Modified();
+		canv->Update();
 	}
 	
 	void Draw(std::string drawOptions = "A")
@@ -47,6 +75,7 @@ public:
 	{
 		return *canv.get();
 	}
+	
 	TMultiGraph& MultiGraph()
 	{
 		return *mg.get();
