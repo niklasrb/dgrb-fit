80fc0b2f92cbf23ea6ad00df0e5751c315eb41d6
Niklas Becker <niklas.becker1@rwth-aachen.de>
2017-08-16 23:05:57 +0200

    some plotting

--- main.cpp
+++ main.cpp
@@ -2,10 +2,18 @@
 #include "LoadFromFiles.h"
 #include "TApplication.h"
 #include "TFile.h"
+#include "TF1.h"
+#include "CanvasWrapper.h"
 //#include "dgrbFit.h"
 
+
+
+
 // g++ -Wall -c "%f" -std=c++11 -I$ROOTSYS/include -g
-// g++ -Wall -o "%e" "%f" $(sh $ROOTSYS/bin/root-config --cflags --glibs) -lgsl -lgslcblas -lm -lncurses -ggdb 
+// g++ -Wall -v -o "%e" "%f" $(sh $ROOTSYS/bin/root-config --cflags --glibs) -lgsl -lgslcblas -L/usr/local/lib/libnest/ -ggdb -lgfortran -lnest3 -llapack 
+
+void plotHaloModel(std::shared_ptr<HaloModel> HM, std::string path);
+
 
 int main(int argc, char** argv)
 {
@@ -23,6 +31,7 @@ int main(int argc, char** argv)
 	//tau->print();
 	
 	auto CM = std::make_shared<LambdaCDM>(Bounds(1e-3, 100), 100);
+	std::cout << "Critical Density: " << CM->CriticalDensity << std::endl;
 	
 	// Load Linear Matter Power Spectrum
 	std::vector<std::fstream> PkFiles;
@@ -35,8 +44,12 @@ int main(int argc, char** argv)
 	auto dNdLogx = LoaddNdLogx();
 	
 	// Prepare the Halo Model
-	auto HM = std::make_shared<HaloModel>(CM, Plin, Bounds(1e-6 /* solar masses*/, 1e18), Bounds(1e-3, 6.), Bounds(1e-2, 1e3));
-	HM->Init(60, 13,  200);
+	auto HM = std::make_shared<HaloModel>(CM, Plin, Bounds(1e-6 /* solar masses*/, 1e18), Bounds(1e-3, 50.), Bounds(1e-2, 1e3));
+	HM->Init(100, 100,  200);
+	
+	if(plot) plotHaloModel(HM, "");
+	
+	return 0;
 	
 	// Define energy bins
 	std::vector<Bounds> EBins = {Bounds(0.1_GeV ,.14_GeV), Bounds(0.14_GeV ,0.2_GeV), Bounds(0.2_GeV ,0.28_GeV), Bounds(0.28_GeV ,0.4_GeV),
@@ -49,7 +62,7 @@ int main(int argc, char** argv)
 	
 	// Prepare Benchmark class
 	
-	Benchmark B(CM, HM, EBins, Bounds(1e-3, 1e15), Bounds(1e-3, 100), Bounds(1e-2, 1e4), true);
+	Benchmark B(CM, HM, EBins, Bounds(1e-3, 1e15), Bounds(1e-3, 10), Bounds(1e-2, 1e3), true);
 
 	std::vector<std::shared_ptr<AstrophysicalSource> > AstrophysicalSources;
 	std::vector<std::shared_ptr<DarkMatter> > dmModels;
@@ -100,3 +113,90 @@ int main(int argc, char** argv)
 	//delete IntensityCanvas;
 	return 0;
 }
+
+void plotHaloModel(std::shared_ptr<HaloModel> HM, std::string path)
+{
+	auto rootFile = new TFile((path + "HaloModelPlots.root").c_str(), "RECREATE", "HaloModelPlots");
+	
+	/// Linear Halo Bias
+	Canvas LinearHaloBias("LHB", "Linear Halo Bias Plot", 1000, 1000);
+	LinearHaloBias().SetLogx(1); LinearHaloBias().SetLogy(1);
+	double* z = new double[4]; z[0]= 0; z[1] = 1; z[2] = 2; z[3] = 3;
+	
+	for(unsigned int i = 0; i < 4; i++)
+	{
+		TF1 LHB(("LHB2z" + std::to_string(z[i])).c_str(), [HM, z, i] (double* args, double* params) { return pow(HM->LinearHaloBias(args[0]*h, z[i]),2.); },
+						1e10, 1e14, 0);
+		LHB.SetNpx(1e5);
+		auto g = new TGraph(&LHB);  g->SetLineColor(i+1);
+		LinearHaloBias.AddGraph(g, ("z = " + std::to_string(int(z[i]))).c_str(), "L");
+	}
+	LinearHaloBias.Draw("A");	
+	LinearHaloBias.SetxLimits(1e10, 1e14);
+	LinearHaloBias.SetyLimits(1e-1, 1e2);
+	LinearHaloBias().BuildLegend();
+	LinearHaloBias().SaveAs("LHB.jpg");
+	
+	/// Halo Mass Function
+	Canvas HMFCanvas("HMF", "Halo Mass Function Plot", 1000, 1000);
+	HMFCanvas().SetLogx(1); HMFCanvas().SetLogy(1);
+	
+	unsigned int n = 30;  double M_min = 1e4;  double M_max = 1e12;
+	double* M = new double[n];
+	for(unsigned int i =0; i < n; i++) M[i] = exp( log(M_min) + i*(log(M_max) - log(M_min))/(n-1.));
+	double* HMF = new double[n];
+	z[0] = 0; z[1] =  10; z[2] = 20; z[3] =  30;
+	for(unsigned int i = 0; i < 4; i++)
+	{
+		for(unsigned int j = 0; j < n; j++) HMF[j] = HM->HaloMassFunction(M[j]*h, z[i]);
+		auto HMFGraph = new TGraph(n, M, HMF);
+		//HMFGraph->SetTitle(("z = " + std::to_string(z[i])).c_str());  
+		HMFGraph->SetLineColor(i+1);
+		//HMFGraph->Draw((std::string(i ==0 ? "A" : "same") + "L").c_str());
+		//auto g = new TGraph(&HMF);  g->SetLineColor(i+1);
+		HMFCanvas.AddGraph(HMFGraph, ("z = " + std::to_string(int(z[i]))).c_str(), "L");
+	}
+	
+	HMFCanvas.Draw("A");	
+	HMFCanvas.SetxLimits(M_min, M_max);
+	HMFCanvas.SetyLimits(1e-7, 1e5);
+	HMFCanvas.SetAxesTitle("M", "dn/dM");
+	HMFCanvas().BuildLegend();
+	HMFCanvas().SaveAs("HMF.jpg");
+	
+	delete []M; delete []HMF;
+	delete []z;
+	
+	/// Integrated Halo Mass Function  
+	/*Canvas IHMFCanvas("nCanvas", "Integrated Halo Mass Function", 1000, 1000);
+	IHMFCanvas().SetLogy(1);
+	n = 40;  z = new double[n]; for(unsigned int i = 0; i < n; i++) z[i] = i*(40.)/(n-1.);
+	int* Mthres = new int[11]; for(unsigned int i = 0; i < 11; i++) Mthres[i] = 4 + i;
+	double* IHMF = new double[n];
+	
+	TF1 _HMF("HMF", [HM] (double* args, double* params) { return exp(args[0])*HM->HaloMassFunction(h*exp(args[0]), params[0]); }, log(1e4), log(1e18), 1);
+	
+	for(unsigned int i = 0; i < 11; i++)
+	{
+		
+		for(unsigned int j =0; j < n; j++) 
+		{
+			_HMF.SetParameters(z[j], 0.);
+			IHMF[j] = _HMF.Integral(log(pow(10., Mthres[i])), log(1e18), 1e-4);
+		}
+		auto IHMFGraph = new TGraph(n, z, IHMF);  IHMFGraph->SetLineColor(i+1);
+		IHMFCanvas.AddGraph(IHMFGraph, "$M_{thres}$ = 1e" + std::to_string(Mthres[i]), "L");
+	}
+	IHMFCanvas.Draw("A");
+	IHMFCanvas.SetyLimits(1e-10, 1e10);
+	IHMFCanvas.SetxLimits(-1, 40);
+	IHMFCanvas().BuildLegend();
+	IHMFCanvas().SaveAs("IHMF.jpg");
+	
+	delete []Mthres; delete []z; */
+	
+	rootFile->Close();
+	delete rootFile;	
+}
+
+
