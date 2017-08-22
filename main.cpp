76a1b8757781a99aa3fd498e783cf1fc407a43cd
Niklas Becker <niklas.becker1@rwth-aachen.de>
2017-08-22 11:24:44 +0200

    plotting

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 // g++ -Wall -v -o "%e" "%f" $(sh $ROOTSYS/bin/root-config --cflags --glibs) -lgsl -lgslcblas -L/usr/local/lib/libnest/ -ggdb -lgfortran -lnest3 -llapack 
 
 void plotHaloModel(std::shared_ptr<HaloModel> HM, std::string path);
+void plotIntensities(std::vector<std::shared_ptr<AstrophysicalSource> >, std::string path);
 
 
 int main(int argc, char** argv)
@@ -20,8 +21,8 @@ int main(int argc, char** argv)
 	bool plot = true;
 	//TFile* plotFile;
 	//if(plot) plotFile = new TFile("plots.root", "RECREATE");
-	TApplication* dummyApp;
-	if(plot) dummyApp = new TApplication("dummy", &argc, argv);
+	//TApplication* dummyApp;
+	//if(plot) dummyApp = new TApplication("dummy", &argc, argv);
 	
 	std::string data("/media/data/Documents/University/BachelorThesis/trafunc/");
 	
@@ -44,15 +45,26 @@ int main(int argc, char** argv)
 	auto dNdLogx = LoaddNdLogx();
 	
 	// Prepare the Halo Model
-	auto HM = std::make_shared<HaloModel>(CM, Plin, Bounds(1e-6 /* solar masses*/, 1e18), Bounds(1e-3, 50.), Bounds(1e-2, 1e3));
-	HM->Init(100, 100,  200);
+	auto HM = std::make_shared<HaloModel>(CM, Plin, Bounds(1e-6 /* solar masses/h*/, 1e18), Bounds(1e-3, 50.), Bounds(1e-2, 1e3));
+	//HM->Init(50, 50,  100);
 	
-	if(plot) plotHaloModel(HM, "");
+	//if(plot) plotHaloModel(HM, "");
 	
-	return 0;
+	//return 0;
 	
-	// Define energy bins
-	std::vector<Bounds> EBins = {Bounds(0.1_GeV ,.14_GeV), Bounds(0.14_GeV ,0.2_GeV), Bounds(0.2_GeV ,0.28_GeV), Bounds(0.28_GeV ,0.4_GeV),
+	auto DT = std::make_shared<FermiLAT>();
+	
+	// Prepare Benchmark class
+	
+	Benchmark B(CM, HM, DT, true, plot);
+	B.LuminosityBounds_global = Bounds(1e-3, 1e15);
+	B.zBounds_global = Bounds(1e-3, 10);  B.zGridLen = 20;
+	B.kBounds_global = Bounds(1e-2, 1e3); B.kGridLen = 200;
+	B.SBounds_global = Bounds(1e-20, 1e-4);  B.SGridLen = 16;
+	B.EBounds_global = Bounds(0.1_GeV, 900._GeV);  B.EGridLen = 20;
+	B.GammaGridLen = 13;
+	
+	B.IntensityBins = {Bounds(0.1_GeV ,.14_GeV), Bounds(0.14_GeV ,0.2_GeV), Bounds(0.2_GeV ,0.28_GeV), Bounds(0.28_GeV ,0.4_GeV),
 								Bounds(0.4_GeV ,0.57_GeV), Bounds(0.57_GeV ,0.8_GeV),  Bounds(0.8_GeV ,1.1_GeV), Bounds(1.1_GeV ,1.6_GeV), 
 								Bounds(1.6_GeV, 2.3_GeV), Bounds(2.3_GeV, 3.2_GeV), Bounds(3.2_GeV, 4.5_GeV), Bounds(4.5_GeV, 6.4_GeV),
 								Bounds(6.4_GeV, 9.1_GeV), Bounds(9.1_GeV, 13._GeV), Bounds(13._GeV, 18._GeV), Bounds(18._GeV, 26._GeV),
@@ -60,39 +72,32 @@ int main(int argc, char** argv)
 								Bounds(100._GeV, 140._GeV), Bounds(140._GeV, 200._GeV), Bounds(200._GeV, 290._GeV), Bounds(290._GeV, 410._GeV),
 								Bounds(410._GeV, 580._GeV), Bounds(580._GeV, 820._GeV)};
 	
-	// Prepare Benchmark class
-	
-	Benchmark B(CM, HM, EBins, Bounds(1e-3, 1e15), Bounds(1e-3, 10), Bounds(1e-2, 1e3), true);
+	B.APSBins = {Bounds(0.5_GeV, 0.72_GeV ), Bounds(0.72_GeV, 1.04_GeV), Bounds(1.04_GeV, 1.38_GeV), Bounds(1.38_GeV, 1.99_GeV), 
+					Bounds(1.99_GeV, 3.15_GeV), Bounds(3.15_GeV, 5.0_GeV), Bounds(5.0_GeV, 7.23_GeV), Bounds(7.23_GeV, 10.24_GeV), 
+					Bounds(10.24_GeV, 21.83_GeV), Bounds(21.83_GeV, 50.0_GeV), Bounds(50.0_GeV, 95.27_GeV), Bounds(95.27_GeV, 199.05_GeV),
+					Bounds(199.05_GeV, 500.0_GeV)  };
 
 	std::vector<std::shared_ptr<AstrophysicalSource> > AstrophysicalSources;
 	std::vector<std::shared_ptr<DarkMatter> > dmModels;
 	
 	
 	//AstrophysicalSources.push_back(std::make_shared<MAGN>(CM, tau));
-	//AstrophysicalSources.push_back(std::make_shared<FSRQ>(CM, tau));
-	//AstrophysicalSources.push_back(std::make_shared<LISP>(CM, tau));
-	//AstrophysicalSources.push_back(std::make_shared<HSP>(CM, tau));
+	AstrophysicalSources.push_back(std::make_shared<FSRQ>(CM, tau));
+	AstrophysicalSources.push_back(std::make_shared<LISP>(CM, tau));
+	AstrophysicalSources.push_back(std::make_shared<HSP>(CM, tau));
 	//AstrophysicalSources.push_back(std::make_shared<NGSFG>(CM, tau));
-	B.calculateIntensityAndAutocorrelationForAstrophysicalSources(AstrophysicalSources, 15, 8); 
+	B.calculateIntensityAndAPSForAstrophysicalSources(AstrophysicalSources); 
 	
 	//dmModels.push_back(std::make_shared<AnnihilatingDM>(CM, HM, tau, dNdLogx, 1e6, 1));
-	dmModels.push_back(std::make_shared<DecayingDM>(CM, HM, tau, dNdLogx, 2e6, 1));
+	//dmModels.push_back(std::make_shared<DecayingDM>(CM, HM, tau, dNdLogx, 2e6, 1));
 	
-	B.calculateIntensityAndAutocorrelationForDM(dmModels, 5, 5);
-	
-	
-	TCanvas* IntensityCanvas = new TCanvas("Intensity", "I", 400, 400);
-	IntensityCanvas->SetLogy();
+	//B.calculateIntensityAndAutocorrelationForDM(dmModels);
 	
 	
-	for(unsigned int i = 0; i < AstrophysicalSources.size(); i++)
-	{
-		AstrophysicalSources.at(i)->printResults(4e-10);
-		auto g = AstrophysicalSources.at(i)->MakeGraph();
-		//g->Print();
-		IntensityCanvas->cd();
-		g->Draw("ALsame");
-	}
+	if(plot) plotIntensities(AstrophysicalSources, "");
+	
+	
+	
 	
 	for(unsigned int i = 0; i < dmModels.size(); i++)
 	{
@@ -101,16 +106,7 @@ int main(int argc, char** argv)
 			std::cout << "[" << dmModels.at(i)->Intensity.at(j).first.first << ", " << dmModels.at(i)->Intensity.at(j).first.second << "]: " << dmModels.at(i)->Intensity.at(j).second << '\t';
 		std::cout << std::endl;
 	}
-	
-	//IntensityCanvas->SaveAs("test.png");
-	if(plot) 
-	{
-		//plotFile->Write();
-		//delete plotFile;
-		dummyApp->Run(true);
-	}
-	
-	//delete IntensityCanvas;
+
 	return 0;
 }
 
@@ -135,7 +131,7 @@ void plotHaloModel(std::shared_ptr<HaloModel> HM, std::string path)
 	LinearHaloBias.SetxLimits(1e10, 1e14);
 	LinearHaloBias.SetyLimits(1e-1, 1e2);
 	LinearHaloBias().BuildLegend();
-	LinearHaloBias().SaveAs("LHB.jpg");
+	LinearHaloBias().SaveAs((path  + "LHB.jpg").c_str());
 	
 	/// Halo Mass Function
 	Canvas HMFCanvas("HMF", "Halo Mass Function Plot", 1000, 1000);
@@ -162,7 +158,7 @@ void plotHaloModel(std::shared_ptr<HaloModel> HM, std::string path)
 	HMFCanvas.SetyLimits(1e-7, 1e5);
 	HMFCanvas.SetAxesTitle("M", "dn/dM");
 	HMFCanvas().BuildLegend();
-	HMFCanvas().SaveAs("HMF.jpg");
+	HMFCanvas().SaveAs((path + "HMF.jpg").c_str());
 	
 	delete []M; delete []HMF;
 	delete []z;
@@ -197,6 +193,21 @@ void plotHaloModel(std::shared_ptr<HaloModel> HM, std::string path)
 	
 	rootFile->Close();
 	delete rootFile;	
+}
+
+void plotIntensities(std::vector<std::shared_ptr<AstrophysicalSource> > sources, std::string path)
+{
+	Canvas IntensityCanvas("Int", "Intensity");
+	IntensityCanvas().SetLogx(1); IntensityCanvas().SetLogy(1); 
+	for(unsigned int i = 0; i < sources.size(); i++)
+	{
+		TGraph* g = new TGraph(sources[i]->MakeGraph());
+		g->SetLineColor(i+1);
+		IntensityCanvas.AddGraph(g, sources[i]->Name, "L");
+	}
+	IntensityCanvas.Draw("A");
+	IntensityCanvas().BuildLegend();
+	IntensityCanvas().SaveAs((path + "ASIntensity.jpg").c_str());
 }
 
 
