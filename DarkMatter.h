a07ca557089aec305a51ab5863f0a26fd05fb621
Niklas Becker <niklas.becker1@rwth-aachen.de>
2017-08-24 20:56:15 +0200

    energy spectra

--- DarkMatter.h
+++ DarkMatter.h
@@ -18,7 +18,7 @@
 class DarkMatter : public DGRBSource
 {
 protected:
-	std::shared_ptr<gsl1DInterpolationWrapper> dNdLogx;
+	std::shared_ptr<gsl2DInterpolationWrapper> dNdLogx;
 
 public:
 	std::shared_ptr<HaloModel> HM;
@@ -28,7 +28,7 @@ public:
 	
 	std::function<double(const double E, const double z)> EnergySpectrum = 0;  // dN/dE
 	
-	DarkMatter(std::shared_ptr<CosmologyModel> _CM, std::shared_ptr<HaloModel> HM, std::shared_ptr<EBLAbsorbtionCoefficient> tau, std::shared_ptr<gsl1DInterpolationWrapper> dNdLogx, const double m, std::string name) : DGRBSource(_CM, tau, name), dNdLogx(dNdLogx), HM(HM), Mass(m)  {}
+	DarkMatter(std::shared_ptr<CosmologyModel> _CM, std::shared_ptr<HaloModel> HM, std::shared_ptr<EBLAbsorbtionCoefficient> tau, std::shared_ptr<gsl2DInterpolationWrapper> dNdLogx, const double m, std::string name) : DGRBSource(_CM, tau, name), dNdLogx(dNdLogx), HM(HM), Mass(m)  {}
 
 };
 
@@ -37,12 +37,12 @@ class AnnihilatingDM : public DarkMatter
 public:
 	const double ThermalizedAnnihilationCrossSection;
 	
-	AnnihilatingDM(std::shared_ptr<CosmologyModel> _CM, std::shared_ptr<HaloModel> HM, std::shared_ptr<EBLAbsorbtionCoefficient> tau, std::shared_ptr<gsl1DInterpolationWrapper> dNdLogx, const double m, const double ThermalizedAnnihilationCrossSection) : DarkMatter(_CM, HM, tau, dNdLogx, m, "Annihilating Dark Matter"), ThermalizedAnnihilationCrossSection(ThermalizedAnnihilationCrossSection)
+	AnnihilatingDM(std::shared_ptr<CosmologyModel> _CM, std::shared_ptr<HaloModel> HM, std::shared_ptr<EBLAbsorbtionCoefficient> tau, std::shared_ptr<gsl2DInterpolationWrapper> dNdLogx, const double m, const double ThermalizedAnnihilationCrossSection) : DarkMatter(_CM, HM, tau, dNdLogx, m, "Annihilating Dark Matter"), ThermalizedAnnihilationCrossSection(ThermalizedAnnihilationCrossSection)
 	{
 		// Set EnergySpectrum
-		EnergySpectrum = [this] ( const double E, const double z)
+		EnergySpectrum = [dNdLogx, m] ( const double E, const double z)
 						{ 	//std::cout << log(2.*E/(e*c_0*c_0*Mass)) << '\t' << this->dNdLogx->Eval(log(2.*E/(e*c_0*c_0*Mass)))/E << std::endl;
-							return this->dNdLogx->Eval(log(2.*E/(e*c_0*c_0*Mass))) /E; };
+							return std::max(0., dNdLogx->Eval(m, log10(2.*E/m))) /E; };
 		
 		WindowFunction = [this] ( const double E, const double z) 
 						{ std::cout << this->ThermalizedAnnihilationCrossSection / (8.*M_PI)  << '\t' <<  pow(CM->O_dm * CM->CriticalDensity / Mass, 2.) * pow(1.+z, 3.) << '\t' <<  this->EnergySpectrum(E*(1.+z), z) << '\t' << exp(-(*(this->tau))(E,z)) << std::endl;
@@ -60,10 +60,10 @@ class DecayingDM : public DarkMatter
 public:
 	const double HalfLife;
 	
-	DecayingDM(std::shared_ptr<CosmologyModel> _CM, std::shared_ptr<HaloModel> HM, std::shared_ptr<EBLAbsorbtionCoefficient> tau, std::shared_ptr<gsl1DInterpolationWrapper> dNdLogx, const double m, const double HalfLife) : DarkMatter(_CM, HM, tau, dNdLogx, m, "Decaying Dark Matter"), HalfLife(HalfLife)
+	DecayingDM(std::shared_ptr<CosmologyModel> _CM, std::shared_ptr<HaloModel> HM, std::shared_ptr<EBLAbsorbtionCoefficient> tau, std::shared_ptr<gsl2DInterpolationWrapper> dNdLogx, const double m, const double HalfLife) : DarkMatter(_CM, HM, tau, dNdLogx, m, "Decaying Dark Matter"), HalfLife(HalfLife)
 	{
-		EnergySpectrum = [this] ( const double E, const double z)
-						{ return this->dNdLogx->Eval(log(1.*E/(e*c_0*c_0*Mass))) /E; };
+		EnergySpectrum = [dNdLogx, m] ( const double E, const double z)
+						{ return std::max(0., dNdLogx->Eval(m, log10(E/m))) /E; };
 		
 		WindowFunction = [this] ( const double E, const double z) 
 						{  return 1. / (4.*M_PI)  * CM->O_dm * CM->CriticalDensity / (Mass*this->HalfLife)* this->EnergySpectrum(E*(1.+z), z) * exp(-(*(this->tau))(E,z)); };
