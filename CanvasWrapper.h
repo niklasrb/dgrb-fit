5dd7c197bb1855f3778b8db14576f0ed4742aabe
Niklas Becker <niklas.becker1@rwth-aachen.de>
2017-08-28 12:46:12 +0200

    fixed axestitle

--- CanvasWrapper.h
+++ CanvasWrapper.h
@@ -27,15 +27,29 @@ protected:
 	std::shared_ptr<TMultiGraph> mg;
 public:
 	
-	Canvas(const std::string name, const std::string title, int height = 500, int width = 500)
+	Canvas(const std::string name, const std::string title, int height = 2000, int width = 2000)
 	{
 		canv = std::make_shared<TCanvas>(name.c_str(), title.c_str(), height, width);
 		mg = std::make_shared<TMultiGraph>(); 
 		//Draw();
 	}
 	
-	void AddGraph(TGraph* g, const std::string title, const std::string drawOptions)
+	int NumberOfGraphs()
 	{
+		if(mg->GetListOfGraphs() == NULL) return 0;
+		else  return mg->GetListOfGraphs()->GetSize();
+	}
+	
+	void AddGraph(TGraph* g, const std::string title, const std::string drawOptions, bool SetLineAttributes = false)
+	{
+		if(SetLineAttributes)
+		{
+			int n = NumberOfGraphs()+1;
+			g->SetLineStyle(n);
+			g->SetLineColor(n);
+			g->SetFillColor(0);
+			g->SetLineWidth(2);
+		}
 		g->SetTitle(title.c_str());
 		mg->Add(g, drawOptions.c_str());
 		Draw();
@@ -59,6 +73,7 @@ public:
 	
 	void SetAxesTitle(std::string xAxis, std::string yAxis)
 	{
+		canv->cd();
 		mg->GetXaxis()->SetTitle(xAxis.c_str());
 		mg->GetYaxis()->SetTitle(yAxis.c_str());
 		canv->Modified();
