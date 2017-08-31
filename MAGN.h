f35519b333afa97bb9c97ba3778402c39521dc5a
Niklas Becker <niklas.becker1@rwth-aachen.de>
2017-08-31 18:58:05 +0200

    testing

--- MAGN.h
+++ MAGN.h
@@ -9,12 +9,13 @@
 class MAGN : public AstrophysicalSource
 {
 protected:
-	double m_E0 = 1* GeV; // <- redefine
+	const double E_0 = 1._GeV; 
 public:
 	MAGN(std::shared_ptr<CosmologyModel> _CM, std::shared_ptr<EBLAbsorbtionCoefficient> tau) : AstrophysicalSource(_CM, tau, std::string("MAGN"))
 	{
-		zBounds.first = 1; zBounds.second = 6;
+		zBounds.first = 0; zBounds.second = 6;
 		GammaBounds.first = 2.37 - 2*0.32; GammaBounds.second = 2.37 + 2*0.32;
+		LumBounds.first = 1e40_ergpers; LumBounds.second = 1e52_ergpers;
 	}
 	
 	double kCorrection(const double E, const double z, const double Gamma) override
@@ -23,45 +24,76 @@ public:
 	}
 	
 	double literal_F(const double E, const double z, const double Gamma) override
-	{
-		double tau = 0; 		// Implement
-		return pow(E/m_E0, -Gamma)*exp(-tau);
+	{	
+		return pow(E/E_0, -Gamma);
 	}
 	
 	double GammaDistribution(const double Gamma) override
 	{
-		return 1/sqrt(2.*M_PI*0.32*0.32) * exp(-(Gamma - 2.37)*(Gamma - 2.37)/(2.*0.32*0.32));
+		return 1./sqrt(2.*M_PI*0.32*0.32) * exp(-pow(Gamma - 2.37, 2.)/(2.*0.32*0.32));
 	}			
 	
-	double LuminosityFunction(const double Luminosity, const double z, const double Gamma) override
+	double LuminosityFunction(const double L, const double z) override
 	{
-		return eta(z)*radioLuminosityFunction( pow(151./1200., 0.2)* pow(10., -22.2/0.77) * pow(0.2*Luminosity, 1./0.77), z)/(0.77*1.008);
+		/*const double k = 3.05;*/
+		const double L_151 =  pow(151./1400.,0.2) * pow(10.,-22.2/0.77) * pow(0.2*L/1._ergpers,(1./0.77));
+		std::cout << "MAGN: L_151 = " << L_151 << std::endl;
+		return /*k*/eta(z)*radioLuminosityFunction( L_151, z)/(0.77*1.008);
 	}
 	
 protected:
 	double eta(const double z)
 	{
-		return pow(c_0, 3)*pow(z*(2+z), 2)/(4.*pow(50.*(1. + z), 3))/CM->ComovingVolume(z);
+		return pow(c_0, 3)*pow(z*(2.+z), 2.)/(4.*pow(50.*(1. + z), 3))/CM->ComovingVolumeElement(z);
 	}
-	double radioLuminosityFunction(const double Luminosity, const double z)
-	{
+	double radioLuminosityFunction(const double L, const double z)	// in erg/s
+	{/*
 		const double rho_lo = pow(10,-7.523);
-		const double L_ls = pow(10,26.48);
+		const double L_ls = pow(10,26.48);		// in erg/s
 		const double alpha_l = 0.586;
 		const double z_lo = 0.71;
 		const double k_1 = 3.48;
 		const double rho_ho = pow(10,-6.757);
 		const double alpha_h = 2.42;
-		const double L_hs = pow(10,27.39);
+		const double L_hs = pow(10,27.39);		// in erg/s
 		const double z_ho = 2.03;
 		const double z_h1 = 0.568;
 		const double z_h2 = 0.956;
 		
-		//std::cout << powf((Luminosity/L_ls),-alpha_l) << '\t' << exp(-Luminosity/L_ls) << '\t' << powf(1+ std::min(z, z_lo),k_1) << std::endl;
-		double rho_l = rho_lo*pow((Luminosity/L_ls),-alpha_l)*exp(-Luminosity/L_ls)*pow(1+ std::min(z, z_lo),k_1);
+		const double rho_l = rho_lo*pow((Luminosity/L_ls),-alpha_l)*exp(-Luminosity/L_ls)*pow(1.+ std::min(z, z_lo),k_1);
 		if(Luminosity <= 1e12) return rho_l;
-		double rho_h = rho_ho*pow((Luminosity/L_hs),-alpha_h)*exp(-L_hs/Luminosity)*exp(-0.5*pow((z-z_ho)/(z >= z_ho ? z_h2 : z_h1), 2.));
-		return rho_l + rho_h;
+		const double rho_h = rho_ho*pow((Luminosity/L_hs),-alpha_h)*exp(-L_hs/Luminosity)*exp(-0.5*pow((z-z_ho)/(z >= z_ho ? z_h2 : z_h1), 2.));
+		return rho_l + rho_h;*/
+		double rho_lo = pow(10,-7.523);
+		double L_ls = pow(10,26.48);
+		double alpha_l = 0.586;
+		double z_lo = 0.71;
+		double k_1 = 3.48;
+		double rho_ho = pow(10,-6.757);
+		double alpha_h = 2.42;
+		double L_hs = pow(10,27.39);
+		double z_ho = 2.03;
+		double z_h1 = 0.568;
+		double z_h2 = 0.956;
+		//definition of the functional dependence
+		double rho_l;
+		if(z<z_lo){
+		rho_l = rho_lo*powf((L/L_ls),-alpha_l)*exp(-L/L_ls)*powf((1+z),k_1);
+		}
+		else if(z>=z_lo){
+		rho_l = rho_lo*powf((L/L_ls),-alpha_l)*exp(-L/L_ls)*powf((1+z_lo),k_1);
+		}
+		double fh;
+		if(z<z_ho){
+		fh = exp(-0.5*powf(((z-z_ho)/z_h1),2));
+		}
+		else if(z>=z_ho){
+		fh = exp(-0.5*powf(((z-z_ho)/z_h2),2));
+		}
+		double rho_h = rho_ho*powf((L/L_hs),-alpha_h)*exp(-L_hs/L)*fh;
+		double retval = rho_l;
+		if(L>1e+12){retval+=rho_h;}
+		return retval;
 	}
 };
 
--- SFG.h
+++ SFG.h
@@ -6,7 +6,16 @@
 
 #include <cmath>
 #include <algorithm>
+#include <tuple>
+#include <string>
 
+
+class SFG;
+class SBSFG;
+class SFGAGN;
+class NGSFG;
+
+/// Star forming Galaxies
 class SFG : public AstrophysicalSource
 {
 protected:
@@ -22,30 +31,34 @@ protected:
 	double z_Phi;
 	double L_IR_0; // maybe obsolete
 	double Phi_s_0;
+	double E_cut = 0.6_GeV;
+	double k;
 	
-	SFG(std::shared_ptr<CosmologyModel> _CM, std::shared_ptr<EBLAbsorbtionCoefficient> tau, std::string name) : AstrophysicalSource(_CM, tau, name)
+	SFG(std::shared_ptr<CosmologyModel> CM, std::shared_ptr<EBLAbsorbtionCoefficient> tau, std::string name) : AstrophysicalSource(CM, tau, name)
 	{
-		zBounds.first = 1; zBounds.second = 6;
+		zBounds.first = 0; zBounds.second = 6;
 		GammaBounds.first = 0; GammaBounds.second = 0;
+		LumBounds.first = 1e20_ergpers; LumBounds.second = 1e52_ergpers;
 	}
 	
 public:
-	SFG(std::shared_ptr<CosmologyModel> _CM, std::shared_ptr<EBLAbsorbtionCoefficient> tau) : SFG(_CM, tau, std::string("SFG")) {}
+	SFG(std::shared_ptr<CosmologyModel> CM, std::shared_ptr<EBLAbsorbtionCoefficient> tau) : SFG(CM, tau, std::string("SFG")) {}
+	
+	SFG(std::shared_ptr<SBSFG> SB, std::shared_ptr<NGSFG> NG, std::shared_ptr<SFGAGN> AGN) ;
 	
 	
 	double kCorrection(const double E, const double z, const double Gamma) override
 	{
-		if(E*(1+z) < 0.6_GeV)
-			return pow(1+z, 2-1.5);
-		if(E < 0.6_GeV  && 0.6_GeV < E*(1+z))
-			return pow(1+z, 2-Gamma_X) * pow(E/0.6_GeV, 1.5 - Gamma_X);
-		//if(0.6_GeV <= E)
-		return pow(1+z, 2-Gamma_X);
+		if(E*(1.+z) < E_cut)
+			return pow(1.+z, 2.-1.5);
+		if(E < E_cut  && E_cut <= E*(1+z))
+			return pow(1.+z, 2.-Gamma_X) * pow(E/E_cut, 1.5 - Gamma_X);
+		return pow(1+z, 2.-Gamma_X);
 	}
 	
 	double literal_F(const double E, const double z, const double Gamma) override
 	{
-		return pow(E/0.6_GeV, ( E < 0.6_GeV ? -1.5 : -Gamma_X ));
+		return pow(E/E_cut, ( E < E_cut ? -1.5 : -Gamma_X ));
 	}
 	
 	double GammaDistribution(const double Gamma) override
@@ -53,35 +66,34 @@ public:
 		return 1;
 	}			
 	
-	
-	double LuminosityFunction(const double Luminosity, const double z, const double Gamma) override
-	{	// this is equivalent to the rescaled luminosity function, because GammaDistribution = 1
-		const double L_solar = 1; // find out
-		return infraredLuminosityFunction( exp(alpha* Luminosity/(L_solar*1e10) + beta), z, Gamma)/alpha; // check
+	double LuminosityFunction(const double L, const double z) override
+	{
+		const double L_IR = 1e10*L_solar  *pow(L/1._ergpers, 1./alpha)*pow(10, -beta/alpha);
+		return k*infraredLuminosityFunction( L_IR, z)/alpha;
 	}
 
 protected:
-	double infraredLuminosityFunction(const double Luminosity, const double z, const double Gamma) // check
+	double infraredLuminosityFunction(const double Luminosity, const double z) 
 	{
 		double L_IR, Phi_s;
 		if(z < z_L) 	
-		{
-			L_IR = L_IR_0 * pow(1+z, k_L1);
-			Phi_s = Phi_s_0 * pow(1+z, k_Phi1);
-		}	else  
-		{
-			L_IR = L_IR_0 * pow(1+z_L, k_L1 - k_L2) * pow(1+z, k_L2);
-			Phi_s = Phi_s_0 * pow(1+z_Phi, k_Phi1 - k_Phi2) * pow(1+z, k_Phi2);
-		}
-		return Phi_s * pow(Luminosity/L_IR, 1-alpha) * exp(-pow(log(1.+ Luminosity/L_IR), 2) /(2*sigma*sigma));
+			L_IR = L_IR_0 * pow(1.+z, k_L1);
+		else  
+			L_IR = L_IR_0 * pow(1.+z_L, k_L1 - k_L2) * pow(1+z, k_L2);
+		
+		if(z < z_Phi)
+			Phi_s = Phi_s_0 * pow(1.+z, k_Phi1);
+		else
+			Phi_s = Phi_s_0 * pow(1.+z_Phi, k_Phi1 - k_Phi2) * pow(1.+z, k_Phi2);
+		return Phi_s * pow(Luminosity/L_IR, 1.-alpha) * exp(-pow(log(1.+ Luminosity/L_IR), 2) /(2*sigma*sigma));
 	}
 
 };
 
-class NGSFG : public SFG
+class NGSFG : public SFG	// normal galaxies that are star forming galaxies
 {
 public:
-	NGSFG(std::shared_ptr<CosmologyModel> _CM, std::shared_ptr<EBLAbsorbtionCoefficient> tau) : SFG(_CM, tau, std::string("NG SFG"))
+	NGSFG(std::shared_ptr<CosmologyModel> CM, std::shared_ptr<EBLAbsorbtionCoefficient> tau) : SFG(CM, tau, std::string("NG SFG"))
 	{
 		alpha = 1.0;
 		beta = 39.28;
@@ -93,32 +105,110 @@ public:
 		k_Phi2 = -7.13;
 		z_Phi = 0.53;
 		Gamma_X = 2.7;
-		L_IR_0 = exp(9.78);
-		Phi_s_0 = exp(-2.12);
+		L_IR_0 = pow(10, 9.78)*L_solar;		
+		Phi_s_0 = pow(10, -2.12)/pow(1._Mpc, 3);
+		
+		k = 1;
 	}
 	
 };
 
-class SBSFG : public SFG
+class SBSFG : public SFG	// star burst star forming galaxies
 {
 public:
-	SBSFG(std::shared_ptr<CosmologyModel> _CM, std::shared_ptr<EBLAbsorbtionCoefficient> tau) : SFG(_CM, tau, std::string("SB SFG"))
+	SBSFG(std::shared_ptr<CosmologyModel> CM, std::shared_ptr<EBLAbsorbtionCoefficient> tau) : SFG(CM, tau, std::string("SB SFG"))
 	{
 		alpha = 1.0;
 		beta = 39.28;
 		sigma = 0.35;
 		k_L1 = 1.96;
 		k_L2 = 0.;
-		z_L = 1.1;
+		z_L = 100;
 		k_Phi1 = 3.97;
 		k_Phi2 = -1.06;
 		z_Phi = 1.1;
 		Gamma_X = 2.2;
-		L_IR_0 = exp(11.17);
-		Phi_s_0 = exp(-4.46);
+		L_IR_0 = pow(10., 11.17)*L_solar;	
+		Phi_s_0 = pow(10., -4.46)/pow(1._Mpc, 3);
+		
+		k = 1e3;
 	}
 	
 };
+
+class SFGAGN : public SFG	// Star forming galaxies with active galactic nuclei
+{
+private:
+	std::shared_ptr<SBSFG> sbsfg;
+	std::shared_ptr<NGSFG> ngsfg;
+	
+	std::vector<std::pair<Bounds, double> > redshift_dependence;
+public:
+	SFGAGN(std::shared_ptr<CosmologyModel> CM, std::shared_ptr<EBLAbsorbtionCoefficient> tau) : SFG(CM, tau, std::string("SFGAGN"))
+	{
+		alpha = 1.20;
+		beta = 39.28;
+		sigma = 0.40;
+		k_L1 = 3.17;
+		k_L2 = 0.;
+		z_L = 100;
+		k_Phi1 = 0.67;
+		k_Phi2 = -3.17;
+		z_Phi = 1.1;
+		L_IR_0 = pow(10, 10.80)*L_solar;	
+		Phi_s_0 = pow(10, -3.2)/pow(1._Mpc, 3);
+		
+		k = 1e3;
+		
+		sbsfg = std::make_shared<SBSFG>(CM, tau);
+		ngsfg = std::make_shared<NGSFG>(CM, tau);
+		zBounds.second = 4.2;
+		redshift_dependence = {std::make_pair(Bounds(0., 0.3), 0.85), std::make_pair(Bounds(0.3, 0.45), 0.91), std::make_pair(Bounds(0.45, 0.6), 0.99),
+								std::make_pair(Bounds(0.6, 0.8), 0.87), std::make_pair(Bounds(0.8, 1.0), 0.73), std::make_pair(Bounds(1.0, 1.2), 0.32),
+								std::make_pair(Bounds(1.2, 1.7), 0.75), std::make_pair(Bounds(1.7, 2.0), 0.75), std::make_pair(Bounds(2.0, 2.5), 0.19),
+								std::make_pair(Bounds(2.5, 3.0), 0.24), std::make_pair(Bounds(3.0, 4.2), 0.28)};
+	}
+	
+
+	double literal_F(const double E, const double z, const double Gamma) override
+	{
+		double sb = sbsfg->literal_F(E, z, Gamma);
+		double ng = ngsfg->literal_F(E, z, Gamma);
+		for(unsigned int i = 0; i < redshift_dependence.size(); i++)
+		{
+			if(redshift_dependence[i].first.first < z && z <= redshift_dependence[i].first.second)  return ng*redshift_dependence[i].second + sb*(1.-redshift_dependence[i].second);
+		}
+		return 0;
+	}
+	
+	double EnergySpectrumOverK(const double E, const double z, const double Gamma) override
+	{
+		double sb = sbsfg->EnergySpectrumOverK(E, z, Gamma);
+		double ng = ngsfg->EnergySpectrumOverK(E, z, Gamma);
+		for(unsigned int i = 0; i < redshift_dependence.size(); i++)
+		{
+			if(redshift_dependence[i].first.first < z && z <= redshift_dependence[i].first.second)  return ng*redshift_dependence[i].second + sb*(1.-redshift_dependence[i].second);
+		}
+		return 0;
+	}
+};
+
+SFG::SFG(std::shared_ptr<SBSFG> SB, std::shared_ptr<NGSFG> NG, std::shared_ptr<SFGAGN> AGN) : SFG(SB->CM, SB->tau, std::string("SFG"))
+{
+	assert(SB->Intensity.size() == NG->Intensity.size() && NG->Intensity.size() == AGN->Intensity.size());
+	assert(SB->APS->Bin1Size() == NG->APS->Bin1Size() && NG->APS->Bin1Size() == AGN->APS->Bin1Size());
+	this->Intensity.resize(SB->Intensity.size());
+	for(unsigned int i = 0; i < Intensity.size(); i++) Intensity[i] = SB->Intensity[i] + NG->Intensity[i] + AGN->Intensity[i];
+	APS = std::make_shared<AstrophysicalSourceAPS<std::shared_ptr<gsl1DInterpolationWrapper> > >(SB->APS->Bin1Size(), SB->APS->Bin2Size());
+	for(unsigned int i = 0; i < SB->APS->Bin1Size(); i++)
+	{
+		for(unsigned int j = 0; j <= i; j++)
+		{
+			APS->at(i, j) = std::make_shared<gsl1DInterpolationWrapper>(*(SB->APS->at(i,j)) + *(NG->APS->at(i,j)) + *(AGN->APS->at(i,j)));
+			if(i!=j) APS->at(j, i) = APS->at(i, j);
+		}
+	}
+}
 
 
 
